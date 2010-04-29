961caa2591b5ad692798b9d66fe745371374e24a
Benoit Goyette <benoitgoyette@gmail.com>
2010-04-29 17:48:20 -0400

    added production.rb

---
+++ config/environments/production.rb
@@ -0,0 +1,22 @@
+Looter::Application.configure do
+  # Settings specified here will take precedence over those in config/environment.rb
+
+  # In the development environment your application's code is reloaded on
+  # every request.  This slows down response time but is perfect for development
+  # since you don't have to restart the webserver when you make code changes.
+  config.cache_classes = true
+
+  # Log error messages when you accidentally call methods on nil.
+  config.whiny_nils = true
+
+  # Show full error reports and disable caching
+  config.consider_all_requests_local       = false
+  config.action_view.debug_rjs             = false
+  config.action_controller.perform_caching = false
+
+  # Don't care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'looter.heroku.com' }
+  config.cookie_secret = 'ea6db5e9c6cd6964d9909cfb6c9101d958ae294b42c23c016739efd27da25e83789642b6a3111c0a2cddc7c1c8ba18d2811b195abceb6ea1e39edbda896a434c'
+  
+end
