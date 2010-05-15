1ad53e33a8b34668c08731ee753c282ce1da2711
Benoit Goyette <benoitgoyette@gmail.com>
2010-05-14 20:21:21 -0400

    corrected campaign/character view

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -5,7 +5,7 @@ class CharactersController < ApplicationController
   # GET /characters
   # GET /characters.xml
   def index
-    @characters = Character.all
+    @characters = @campaign.characters
 
     respond_to do |format|
       format.html # index.html.erb
